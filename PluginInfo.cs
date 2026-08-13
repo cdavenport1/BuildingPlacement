a1e344883a76944fcf8c46cdcbdd867d02572e32
cdavenport1 <cdavenport2345@gmail.com>
2026-08-12 22:06:09 -0400

    Fixing versioning issue with the plugin.

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -1,8 +1,7 @@
 namespace NuclearOptionCommander;
 
-internal static class PluginInfo
+internal static partial class PluginInfo
 {
     public const string Guid = "com.nuclearoption.standalonebuildingplacement";
     public const string Name = "Standalone Building Placement";
-    public const string Version = "0.1.2.0";
 }
