a6dbf452fed4f495a9996491f851c6fc7b7b1a0a
cdavenport1 <cdavenport2345@gmail.com>
2026-08-12 23:31:26 -0400

    Removing duplicate folder

--- StandaloneBuildingPlacement/PluginInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace NuclearOptionCommander;
-
-internal static class PluginInfo
-{
-    public const string Guid = "com.nuclearoption.standalonebuildingplacement";
-    public const string Name = "Standalone Building Placement";
-    public const string Version = "0.1.2.0";
-}
